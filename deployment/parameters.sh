0c7de89010459c879d96e7031dc5d2469a3e503c
Cody Brown <cody2094@hotmail.com>
2023-08-28 16:34:15 -0500

    feat(team): add deployment parameters to our forked repo

---
+++ deployment/parameters.sh
@@ -0,0 +1,23 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+EMAIL_SOURCE=dev@usehyphen.com
+IDC_LOGIN_URL=https://d-9067b82597.awsapps.com/start
+REGION=us-east-1
+TEAM_ACCOUNT=200986924166
+ORG_MASTER_PROFILE=pulumi
+TEAM_ACCOUNT_PROFILE=pulumi-identity
+TEAM_ADMIN_GROUP=admins
+TEAM_AUDITOR_GROUP=auditors
+TAGS="project=iam-identity-center-team environment=prod"
